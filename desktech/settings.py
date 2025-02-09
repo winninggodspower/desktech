3316885d217b0991824071d7382f924625ee00f1
winnning godspower <winnninggodspower@gmail.com>
2025-02-09 04:43:50 +0100

    feat: update STATIC_ROOT path in settings.py for deployment

--- desktech/settings.py
+++ desktech/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "../../desktech.pro/static")
 
 
 # Default primary key field type
