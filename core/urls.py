4f508776f2c1b8aeb658d434c90c15d744054856
winnning godspower <winnninggodspower@gmail.com>
2025-02-20 00:16:13 +0100

    feat: add about and contact pages with navigation updates

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("training", views.training, name="training")
+    path("training", views.training, name="training"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -9,3 +9,6 @@ def training(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact_us.html')
